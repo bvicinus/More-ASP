960fc9bb061c3f709b93c579145d2cc91604498c
Brandon A. Vicinus <bvicinus@mail.usf.edu>
2016-01-28 12:43:40 -0500

    1st commit

---
+++ Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hello World</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label2" runat="server" Text="Please enter your name then click OK."></asp:Label>
+        <br />
+        <asp:TextBox ID="tbName" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnOK" runat="server" OnClick="btnOK_Click" Text="OK" 
+            Width="88px" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnCancel" runat="server" onclick="btnCancel_Click" 
+            Text="Cancel" Width="88px" />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+        <asp:Label ID="lblGreeting" runat="server" Font-Bold="True" Font-Names="Arial" 
+            Font-Size="XX-Large" ForeColor="Red" Text="Hello, World!"></asp:Label>
+    
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="lblCount" runat="server" BackColor="#00CC00" Text="Count = 0"></asp:Label>
+    
+
+        <br /> 
+        <br />
+
+        <% Write_Start_Time(); %>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private int count = 0;
+
+
+    //user added
+    public void Write_Start_Time()
+    {
+        //picking up the application variable start_time
+        DateTime startTime = (DateTime)Application["Start_Time"];
+
+        //send something down to the browser
+        //'puts its parameter into the output stream going to the browser'
+        Response.Write("This application was started at " + startTime.ToString());
+    }
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        //each page load, incriment count 
+        //if this session/viewstate count hasnt been created, its null
+        if (ViewState["ct"] == null) 
+        {
+            ViewState["ct"] = 0; //and needs to be created, here
+        }
+        count = (int)ViewState["ct"]; //need to type cast to get back an actual int
+        ViewState["ct"] = ++count;
+
+        //session state is retained in memory on the server 
+        //for same tabs, and browser windows
+
+        //viewstate will make a new viewstate variable for each tab!!
+        //seperate instance of viewstate in every page
+
+
+        if (!IsPostBack) //only on original page load 
+        {
+            lblGreeting.Text = "It's a new world!";
+            lblGreeting.ForeColor = System.Drawing.Color.Blue;
+        }
+    }
+
+
+    //button event for OK
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        lblGreeting.Text = "Hello, " + tbName.Text; //change the name
+        lblCount.Text = "Count = " + count.ToString();
+        //need to call toString on the int 
+
+    }
+
+    //button event for CANCEL
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        tbName.Text = ""; //clear the text box 
+        lblGreeting.Text = ""; //clear the string
+        lblCount.Text = "Count = " + count.ToString();
+        
+    }
+}
